373930a5fdebc21b2f52a4358a9f17e5e765080c
Emanuel Ralha <ejr@netmais.local>
2016-11-04 11:32:47 +0000

    New logic for rooms, testing performance.

--- server/bin.js
+++ server/bin.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 var app = express();
 var server = require('http');
 var path = require('path');
--- server/socket.js
+++ server/socket.js
@@ -5,14 +5,38 @@ module.exports = function (server) {
 
 	var rtc = io.of('/rtc').on('connection', function (socket) {
 
-	  socket.on('emit', function (data) {
+		/*
+			console.log('---------------------------------');
+			for(i in rtc.connected){
+				if(socket.id == rtc.connected[i].id){ console.log('socket found', rtc.connected[i].id); }
+				console.log(rtc.connected[i].id);
+			}
+		*/
 
-	  	console.log(data);
+		  socket.on('joinRoom', function (room) {
+		  	socket.emit('msg', 'room joined');
+		  	socket.join(room);
+		  });
 
-	  	//rtc.emit('msg', data); //everyone in RTC chanel will get this, even the socket calling it
-	    socket.broadcast.emit('msg', data);
+		  socket.on('leaveRoom', function (room) {
+		  	socket.emit('msg', 'room leaved');
+		  	socket.leave(room);
+		  });
 
-	  });
+		  socket.on('emitRoom', function (data) {
+		  	//console.log(data, rtc.adapter.rooms);
+		  	data.id = socket.client.id;
+		  	rtc.to(data.room).emit('msg', data);
+		  });
+
+		  socket.on('emit', function (data) {
+
+		  	console.log(data);
+
+		  	//rtc.emit('msg', data); //everyone in RTC chanel will get this, even the socket calling it
+		    socket.broadcast.emit('msg', data);
+
+		  });
 
 	});
 
